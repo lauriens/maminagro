ad3576fb57a88937e79a33ca6d0475d5c1b32f25
Laurien <laurien.santin@poatek.com>
2024-01-11 11:43:08 -0300

    test fix datetime mappings

--- BancoDeEspecies.Application/Mappings/OccurrenceMap.cs
+++ BancoDeEspecies.Application/Mappings/OccurrenceMap.cs
@@ -8,10 +8,20 @@ namespace BancoDeEspecies.Application.Mappings
     {
         public OccurrenceMap()
         {
+            CreateMap<DateTime?, DateTime?>().ConvertUsing(new DateTimeTypeConverter());
             CreateMap<Occurrence, OccurrenceViewModel>()
                 .ForMember(o => o.OccurrenceType, opt => opt.MapFrom(src => src.OccurrenceType.ToString()));
             CreateMap<CreateOccurrenceViewModel, Occurrence>();
             CreateMap<EditOccurrenceViewModel, Occurrence>();
+        }
+    }
+
+    public class DateTimeTypeConverter : ITypeConverter<DateTime?, DateTime?>
+    {
+        DateTime? ITypeConverter<DateTime?, DateTime?>.Convert(DateTime? source, DateTime? destination, ResolutionContext context)
+        {
+            if (source == null) return null;
+            return DateTime.SpecifyKind(source.Value, DateTimeKind.Utc);
         }
     }
 }
